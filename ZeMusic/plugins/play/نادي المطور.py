e38d06ad9b46306d59016794b8c5d501edc1452d
bxbkl <167901453+bxbkl@users.noreply.github.com>
2024-05-05 08:14:50 +0300

    Update نادي المطور.py

--- ZeMusic/plugins/play/نادي المطور.py
+++ ZeMusic/plugins/play/نادي المطور.py
@@ -42,7 +42,10 @@ async def call_dev(client: Client, message: Message):
                                      f"<b>⌯ يوزر المجموعه {chatusername}</b>",
                                      reply_markup=reply_markup)
 
+    # "انشاء زر "اونلاين"  
+    online_button = InlineKeyboardButton(mname, url=f"https://t.me/{musrnam}")
+
     
-    await message.reply_text(f"<b>  • تم إرسال طلبك للمطور سيتم الرد عليك قريباً .   \n\n  -› {} .</b>",
+    await message.reply_text(f"<b>  • تم إرسال طلبك للمطور سيتم الرد عليك قريباً .   \n\n 𝑫𝒆𝒗 -› @{musrnam} .</b>",
                              disable_web_page_preview=True,
                              reply_markup=InlineKeyboardMarkup([[online_button]]))
